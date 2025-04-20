c26b9436f12ff6d69bbe4fd6f45bc818e2a91ed7
mariots <mario_ts@outlook.com>
2025-04-19 22:06:17 -0600

    Get PrismaClient for generated client

--- src/database/prismaClientFactory.ts
+++ src/database/prismaClientFactory.ts
@@ -1,13 +1,13 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient as TenantPrismaClient } from "../../prisma/tenant/generated/client";
 import { getTenantDatabaseUrl } from "./utils";
 
-const clients: Record<string, PrismaClient> = {};
+const clients: Record<string, TenantPrismaClient> = {};
 
-export function getPrismaClient(tenantDbName: string): PrismaClient {
+export function getPrismaClient(tenantDbName: string): TenantPrismaClient {
   if (!clients[tenantDbName]) {
     const url = getTenantDatabaseUrl(tenantDbName);
 
-    clients[tenantDbName] = new PrismaClient({
+    clients[tenantDbName] = new TenantPrismaClient({
       datasources: { db: { url } },
     });
   }
@@ -15,4 +15,4 @@ export function getPrismaClient(tenantDbName: string): PrismaClient {
   return clients[tenantDbName];
 }
 
-export const centralPrisma = new PrismaClient();
+export const centralPrisma = new TenantPrismaClient();
